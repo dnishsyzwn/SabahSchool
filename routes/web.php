86ac94a094c20445a24d8a905d4ba61b8f7214a3
Azim Amin <azimamin@Zyms-MacBook-Air.local>
2026-04-15 16:47:54 +0800

    predeployment

--- routes/web.php
+++ routes/web.php
@@ -99,3 +99,14 @@ Route::prefix('admin')->name('admin.')->middleware(['admin'])->group(function ()
     Route::patch('/form-submissions/{formSubmission}/status', [\App\Http\Controllers\Admin\FormSubmissionController::class, 'updateStatus'])->name('form-submissions.update-status');
     Route::delete('/form-submissions/{formSubmission}', [\App\Http\Controllers\Admin\FormSubmissionController::class, 'destroy'])->name('form-submissions.destroy');
 });
+
+// Deployment Routes
+Route::get('/run-migrate', function () {
+    \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
+    return "Migration completed!";
+});
+
+Route::get('/run-link', function () {
+    \Illuminate\Support\Facades\Artisan::call('storage:link');
+    return "Storage linked!";
+});
